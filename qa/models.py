8e0687b47bafab0c1c34211a6389c409ac6b9262
Ruslan Matveev <rumatveev@protonmail.com>
2019-03-12 13:06:52 +0300

    Addede QA Models

--- qa/models.py
+++ qa/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
+class Question(models.Model):
+    title = models.CharField(blank=False, null=False, max_length=255)
+    text = models.TextField()
+    added_at = models.DateField(auto_now=True)
+    rating = models.IntegerField(default=0)
+    author = models.ForeignKey(CustomUser, on_delete=models.SET(value='Deleted'))
+    likes = models.ManyToManyField(CustomUser, related_name='get_likes', default=0)
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+    def get_url(self):
+        return "/question/{}".format(self.id)
+
+
+class Answer(models.Model):
+    added_at = models.DateField(auto_now=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.SET(value='Deleted'))
+    text = models.TextField(null=True)
+
+    def __str__(self):
+        return self.text
