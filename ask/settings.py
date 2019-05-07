270dc2980c7b95e7edcceab66549521cc2009dfd
Ruslan Matveev <rumatveev@protonmail.com>
2019-05-07 16:26:53 +0300

    cleaning fixtures

--- ask/settings.py
+++ ask/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 AUTH_USER_MODEL = 'users.CustomUser'  # adds custom user auth model
 LOGIN_REDIRECT_URL = 'home'
