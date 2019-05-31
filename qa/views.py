7b5a676e0e4af4a2f45c691a3fd76f0711775ed0
Ruslan Matveev <rumatveev@protonmail.com>
2019-05-31 17:28:14 +0300

    Heroku Settings

--- qa/views.py
+++ qa/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404, HttpResponseRedirect
-from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
