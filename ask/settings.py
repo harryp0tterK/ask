04e101f524e431ac4e5ec14373d8d949896abac9
harryp0tterK <95028526+harryp0tterK@users.noreply.github.com>
2022-06-03 08:31:35 +0900

    change_setting_mysql

--- ask/settings.py
+++ ask/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '', # プロジェクトで使用するデータベース名
+        'USER': '', # パソコンにインストールしたMySQLのユーザー名
+        'PASSWORD': '', # 同上。そのパスワード
     }
 }
 
